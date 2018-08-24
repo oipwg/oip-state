cd54645789b39f9ad26dce3061e4bab457a31524
Sky Young <sky@alexandria.io>
2018-08-23 17:44:32 -0700

    Fix fetching artifact error because of callback

--- src/actions/ActiveArtifact/thunks.js
+++ src/actions/ActiveArtifact/thunks.js
@@ -1,7 +1,7 @@
 import {
-    fetchingArtifact,
-    _setActiveArtifact,
-    fetchArtifactError
+	fetchingArtifact,
+	_setActiveArtifact,
+	fetchArtifactError
 } from "./actions";
 
 import { setActiveFile, addToActiveFiles } from '../ActiveArtifactFiles/thunks'
@@ -10,34 +10,37 @@ import { setActiveFile, addToActiveFiles } from '../ActiveArtifactFiles/thunks'
 // SELECT CURRENT ARTIFACT
 
 export const setActiveArtifact = (artifact, callback) => (dispatch) => {
-    // Set the Active Artifact
-    dispatch(_setActiveArtifact(artifact))
+	// Set the Active Artifact
+	dispatch(_setActiveArtifact(artifact))
 
-    // Get all the files in the Artifact
-    let files = artifact.getFiles()
-    // Add all Files to the ActiveFiles
-    for (let i = 0; i < files.length; i++){
-        dispatch(addToActiveFiles(artifact, files[i]))
-    }
+	// Get all the files in the Artifact
+	let files = artifact.getFiles()
+	// Add all Files to the ActiveFiles
+	for (let i = 0; i < files.length; i++){
+		dispatch(addToActiveFiles(artifact, files[i]))
+	}
 
-    // Set the first file to be active (by default)
-    dispatch(setActiveFile(artifact, files[0]))
+	// Set the first file to be active (by default)
+	dispatch(setActiveFile(artifact, files[0]))
 
-    // Pass back the artifact to a callback if provided
-    if (callback)
-        callback(artifact)
+	// Pass back the artifact to a callback if provided
+	if (callback)
+		callback(artifact)
 }
 
 export const loadActiveArtifact = (txid, callback) => async (dispatch, getState) => {
-    dispatch(fetchingArtifact());
+	dispatch(fetchingArtifact());
 
-    let state = getState();
-    try {
-        let artifact = await state.OIPIndex.Index.getArtifact(txid)
-        // Set the Active Artifact
-        dispatch(setActiveArtifact(artifact, callback))
-    } catch (e) {
-        dispatch(fetchArtifactError("Error fetching Artifact!"))
-        console.error(e)
-    }
+	let state = getState();
+
+	let artifact
+	try {
+		artifact = await state.OIPIndex.Index.getArtifact(txid)
+	} catch (e) {
+		dispatch(fetchArtifactError("Error fetching Artifact!"))
+		console.error(e)
+	}
+
+	// Set the Active Artifact
+	dispatch(setActiveArtifact(artifact, callback))
 };
